6550db4bcf6aa51e9e537e5d68abcd36ddf7e2bf
Sami Siraj <naimoon6450@gmail.com>
2019-12-11 23:44:58 -0500

    Updates

---
+++ precache-manifest.04626ef1ecc7edd7736364f495cb8d7b.js
@@ -0,0 +1,22 @@
+self.__precacheManifest = [
+  {
+    "revision": "a21b3145e3cb19b5553a",
+    "url": "/nsiraj-pfolio/static/js/runtime~main.a21b3145.js"
+  },
+  {
+    "revision": "f2df8f953a9ca316b2b1",
+    "url": "/nsiraj-pfolio/static/js/main.f2df8f95.chunk.js"
+  },
+  {
+    "revision": "cee8f1bc3895b142bd37",
+    "url": "/nsiraj-pfolio/static/js/2.cee8f1bc.chunk.js"
+  },
+  {
+    "revision": "f2df8f953a9ca316b2b1",
+    "url": "/nsiraj-pfolio/static/css/main.0fa75113.chunk.css"
+  },
+  {
+    "revision": "6a6c4d9d7e87c746acd1c4ec06dc1370",
+    "url": "/nsiraj-pfolio/index.html"
+  }
+];
