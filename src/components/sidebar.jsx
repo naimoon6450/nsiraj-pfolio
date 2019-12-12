f0f03d00d4229aabeff7e090e248e70446dd60c4
Sami Siraj <naimoon6450@gmail.com>
2019-12-11 22:14:14 -0500

    updated sidebar

--- src/components/sidebar.jsx
+++ src/components/sidebar.jsx
@@ -6,90 +6,90 @@ export default class Sidebar extends Component {
     return (
       <div>
         <nav
-          href='#navbar'
-          className='js-colorlib-nav-toggle colorlib-nav-toggle'
-          data-toggle='collapse'
-          data-target='#navbar'
-          aria-expanded='false'
-          aria-controls='navbar'
+          href="#navbar"
+          className="js-colorlib-nav-toggle colorlib-nav-toggle"
+          data-toggle="collapse"
+          data-target="#navbar"
+          aria-expanded="false"
+          aria-controls="navbar"
         >
           <i />
         </nav>
 
-        <aside id='colorlib-aside' className='border js-fullheight'>
-          <Particles className='particles' params={partOptions} />
-          <div className='text-center'>
+        <aside id="colorlib-aside" className="border js-fullheight">
+          <Particles className="particles" params={partOptions} />
+          <div className="text-center">
             <div
-              className='author-img'
+              className="author-img"
               style={{
                 backgroundImage: 'url(images/ns_va.jpg)',
                 backgroundSize: 'cover',
-                backgroundPosition: 'left'
+                backgroundPosition: 'left',
               }}
             />
           </div>
-          <nav id='colorlib-main-menu' role='navigation' className='navbar'>
-            <div id='navbar' className='collapse'>
+          <nav id="colorlib-main-menu" role="navigation" className="navbar">
+            <div id="navbar" className="collapse">
               <ul>
-                <li className='active'>
-                  <a href='#home' data-nav-section='home'>
+                <li className="active">
+                  <a href="#home" data-nav-section="home">
                     Introduction
                   </a>
                 </li>
                 <li>
-                  <a href='#about' data-nav-section='about'>
+                  <a href="#about" data-nav-section="about">
                     About
                   </a>
                 </li>
                 <li>
-                  <a href='#experience' data-nav-section='experience'>
+                  <a href="#experience" data-nav-section="experience">
                     Experience
                   </a>
                 </li>
               </ul>
             </div>
           </nav>
-          <nav id='colorlib-main-menu'>
+          <nav id="colorlib-main-menu">
             <ul>
               <li>
                 <a
-                  href='https://www.instagram.com/naimunsiraj/'
-                  target='_blank'
-                  rel='noopener noreferrer'
+                  href="https://www.instagram.com/naimunsiraj/"
+                  target="_blank"
+                  rel="noopener noreferrer"
                 >
-                  <i className='icon-instagram' />
+                  <i className="icon-instagram" />
                 </a>
               </li>
               <li>
                 <a
-                  href='https://www.linkedin.com/in/nsiraj/'
-                  target='_blank'
-                  rel='noopener noreferrer'
+                  href="https://www.linkedin.com/in/nsiraj/"
+                  target="_blank"
+                  rel="noopener noreferrer"
                 >
-                  <i className='icon-linkedin2' />
+                  <i className="icon-linkedin2" />
                 </a>
               </li>
               <li>
                 <a
-                  href='https://github.com/naimoon6450'
-                  target='_blank'
-                  rel='noopener noreferrer'
+                  href="https://github.com/naimoon6450"
+                  target="_blank"
+                  rel="noopener noreferrer"
                 >
-                  <i className='icon-github'></i>
+                  <i className="icon-github"></i>
                 </a>
               </li>
               <li>
                 <a
-                  href='https://naimunsiraj.wordpress.com'
-                  target='_blank'
-                  rel='noopener noreferrer'
+                  href="https://naimunsiraj.wordpress.com"
+                  target="_blank"
+                  rel="noopener noreferrer"
                 >
-                  <i className='icon-wordpress'></i>
+                  <i className="icon-wordpress"></i>
                 </a>
               </li>
             </ul>
           </nav>
-          <div className='colorlib-footer'></div>
+          {/* <div className='colorlib-footer'></div> */}
         </aside>
       </div>
     );
@@ -103,35 +103,35 @@ export const partOptions = {
       value: 80,
       density: {
         enable: true,
-        value_area: 800
-      }
+        value_area: 800,
+      },
     },
     color: {
-      value: '#000000'
+      value: '#000000',
     },
     line_linked: {
-      color: '#708090'
+      color: '#708090',
     },
     shape: {
-      type: 'circle'
+      type: 'circle',
     },
     size: {
-      value: 2
-    }
+      value: 2,
+    },
   },
   interactivity: {
     detect_on: 'window',
     events: {
       onhover: {
         enable: true,
-        mode: 'repulse'
-      }
+        mode: 'repulse',
+      },
     },
     modes: {
       repulse: {
         distance: 50,
-        duration: 0.4
-      }
-    }
-  }
+        duration: 0.4,
+      },
+    },
+  },
 };
--- src/components/timeline.jsx
+++ src/components/timeline.jsx
@@ -4,99 +4,99 @@ export default class Timeline extends Component {
   render() {
     return (
       <div>
-        <section className='colorlib-experience' data-section='experience'>
-          <div className='colorlib-narrow-content'>
-            <div className='row'>
+        <section className="colorlib-experience" data-section="experience">
+          <div className="colorlib-narrow-content">
+            <div className="row">
               <div
-                className='col-md-6 col-md-offset-3 col-md-pull-3 animate-box'
-                data-animate-effect='fadeInLeft'
+                className="col-md-6 col-md-offset-3 col-md-pull-3 animate-box"
+                data-animate-effect="fadeInLeft"
               >
-                <span className='heading-meta'>Experience</span>
+                <span className="heading-meta">Experience</span>
                 {/* <h2 className="colorlib-heading animate-box">Timeline</h2> */}
               </div>
             </div>
-            <div className='row'>
-              <div className='col-md-12'>
-                <div className='timeline-centered'>
+            <div className="row">
+              <div className="col-md-12">
+                <div className="timeline-centered">
                   <article
-                    className='timeline-entry animate-box'
-                    data-animate-effect='fadeInLeft'
+                    className="timeline-entry animate-box"
+                    data-animate-effect="fadeInLeft"
                   >
-                    <div className='timeline-entry-inner'>
+                    <div className="timeline-entry-inner">
                       <div
-                        className='timeline-icon color-3'
+                        className="timeline-icon color-3"
                         style={{
                           backgroundImage: 'url(images/acn_logo.png)',
                           backgroundSize: 'cover',
-                          backgroundPosition: 'left'
+                          backgroundPosition: 'left',
                         }}
                       >
                         {/* <i className='icon-pen2' /> */}
                       </div>
-                      <div className='timeline-label'>
+                      <div className="timeline-label">
                         <h2>
                           Accenture
-                          <span className='durations'>2015 - Present</span>
+                          <span className="durations">2015 - Present</span>
                         </h2>
-                        <h6 className='roles'>
+                        <h6 className="roles">
                           Cloud Migration & Software Engineering Consultant
                         </h6>
                       </div>
                     </div>
                   </article>
                   <article
-                    className='timeline-entry animate-box'
-                    data-animate-effect='fadeInTop'
+                    className="timeline-entry animate-box"
+                    data-animate-effect="fadeInTop"
                   >
-                    <div className='timeline-entry-inner'>
+                    <div className="timeline-entry-inner">
                       <div
-                        className='timeline-icon color-4'
+                        className="timeline-icon color-4"
                         style={{
                           backgroundImage: 'url(images/fsa.jpg)',
                           backgroundSize: 'cover',
-                          backgroundPosition: 'left'
+                          backgroundPosition: 'left',
                         }}
                       ></div>
-                      <div className='timeline-label'>
+                      <div className="timeline-label">
                         <h2>
                           Fullstack Academy
-                          <span className='durations'>2019</span>
+                          <span className="durations">2019</span>
                         </h2>
-                        <h6 className='roles'>
+                        <h6 className="roles">
                           Part-Time (Flex) Program | NERP Stack
                         </h6>
                       </div>
                     </div>
                   </article>
                   <article
-                    className='timeline-entry animate-box'
-                    data-animate-effect='fadeInTop'
+                    className="timeline-entry animate-box"
+                    data-animate-effect="fadeInTop"
                   >
-                    <div className='timeline-entry-inner'>
+                    <div className="timeline-entry-inner">
                       <div
-                        className='timeline-icon color-4'
+                        className="timeline-icon color-4"
                         style={{
                           backgroundImage: 'url(images/cu_logo.png)',
                           backgroundSize: 'cover',
-                          backgroundPosition: 'left'
+                          backgroundPosition: 'left',
                         }}
                       ></div>
-                      <div className='timeline-label'>
+                      <div className="timeline-label">
                         <h2>
                           Columbia University, School of Engineering & Applied
                           Sciences (SEAS)
-                          <span className='durations'>2011 - 2015</span>
+                          <span className="durations">2011 - 2015</span>
                         </h2>
-                        <h6 className='roles'>B.S. Chemical Engineering</h6>
+                        <h6 className="roles">B.S. Chemical Engineering</h6>
                       </div>
                     </div>
                   </article>
                   <article
-                    className='timeline-entry begin animate-box'
-                    data-animate-effect='fadeInBottom'
+                    className="timeline-entry begin animate-box"
+                    data-animate-effect="fadeInBottom"
                   >
-                    <div className='timeline-entry-inner'>
-                      <div className='timeline-icon color-none'></div>
+                    <div className="timeline-entry-inner">
+                      <div className="timeline-icon color-none"></div>
                     </div>
                   </article>
                 </div>
